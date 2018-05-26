287e09c86a7c93d170aa0437e373a2b3112195bd
jude-LafiteIII <judewrs@gmail.com>
2018-05-26 15:24:20 +0100

    registration form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        if len(username) > 32:
+            raise ValidationError('Please enter a username less than 32 characters')
+
+        user = User.query.filter_by(username=username.data).first()
+
+        if user is not None:
+            raise ValidationError('Please use a different username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+
+        if user is not None:
+            raise ValidationError('Email already in use')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
 from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from app import db
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
 
     posts = [
@@ -38,10 +41,37 @@ def login():
 
             else:
                 login_user(user, remember=form.remember_me.data)
-                return redirect(url_for('index'))
+
+                next_page = request.args.get('next')
+
+                if not next_page or url_parse(next_page).netloc != '':
+                    next_page = url_for('index')
+                return redirect(next_page)
 
         else:
             return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    else:
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            user = User(username=form.username.data, email=form.email.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+
+            login_user(user)
+            
+            flash('Thank you for registering, {}'.format(user.username))
+
+            return redirect(url_for('index'))
+
+        else:
+            return render_template('register.html', title='Register', form=form)
 
 @app.route('/logout')
 def logout():
