1bc91c5cf59b982d40d20b9df5059f8c01320309
jude-LafiteIII <judewrs@gmail.com>
2018-06-21 20:50:40 +0100

    avatars n stuff

--- config.py
+++ config.py
@@ -6,6 +6,3 @@ class Config(object):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    DISCORD_OAUTH_SECRET_KEY = 'FjMSodOATz4FoXByylp6QS0lWQLeYKWa'
-    DISCORD_OAUTH_CLIENT_ID = '440052776702312450'
