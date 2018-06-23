1703840b2204a22425fa41d8e3f8aa9c9fe3de61
jude-LafiteIII <judewrs@gmail.com>
2018-06-23 14:35:41 +0100

    linked up the last seen date

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,14 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from app import db
+from datetime import datetime
 
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
