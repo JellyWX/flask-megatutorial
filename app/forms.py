d200702387fe4021a395cff50c3f9d6460c06a01
jude-LafiteIII <judewrs@gmail.com>
2018-06-25 23:14:05 +0100

    profile editing

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TestAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
@@ -34,9 +34,3 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=1, max=32)])
     about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-
-        if user is not None:
-            raise ValidationError('Please use a different username')
