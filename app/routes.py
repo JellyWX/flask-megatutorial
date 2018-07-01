57325b207318d85e134954a5e1d775e1ac1611a7
jude-LafiteIII <judewrs@gmail.com>
2018-07-01 15:53:19 +0100

    previous/next buttons

--- app/routes.py
+++ app/routes.py
@@ -38,16 +38,25 @@ def index():
         flash('Your post has been added.')
         return redirect(url_for('index'))
 
-    posts = current_user.followed_posts().all()
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
 
-    return render_template('index.html', title='Home', user=current_user, posts=posts, form=form)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+
+    return render_template('index.html', title='Home', user=current_user, posts=posts.items, form=form, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts, user=current_user)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+
+    return render_template('index.html', title='Explore', posts=posts.items, user=current_user, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
